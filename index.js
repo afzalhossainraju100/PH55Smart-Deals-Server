11f3bd904931e0fbd13209733c5c3a5651a864fd
afzalhossainraju100 <raju23105341031@diu.edu.bd>
2026-02-17 01:36:17 +0600

    connenct the server with mongodb and piked

--- index.js
+++ index.js
@@ -1,16 +1,56 @@
 const express = require("express");
 const cors = require('cors');
 const app = express();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const port =process.env.PORT || 5000;
 
 //middleware
 app.use(cors());
 app.use(express.json())
 
+
+const uri =
+  "mongodb+srv://SmartDBUser:AVlBw6RkzWOoWCrK@cluster0.1ezipje.mongodb.net/?appName=Cluster0";
+
+  const client = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    },
+  });
+
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+
+async function run() {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    //await client.close();
+  }
+}
+run().catch(console.dir);
+
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+
+
+
+// client.connect()
+// .then(()=>{
+//   app.listen(port, () => {
+//     console.log(`Example app listening on port ${port}`);
+//   });
+// })
+// .catch(console.dir)
